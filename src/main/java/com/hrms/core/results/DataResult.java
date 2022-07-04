347297a62a5e6b032db20df814378230583c10f8
Kaan Şenel <tufankaansenel@gmail.com>
2022-07-04 15:20:02 +0300

    Result updated.

---
+++ src/main/java/com/hrms/core/results/DataResult.java
@@ -0,0 +1,23 @@
+package com.hrms.core.results;
+
+import lombok.Getter;
+
+@Getter
+public class DataResult<T> extends Result {
+
+    private T data;
+    public DataResult(T data,boolean success,String message) {
+        super(success,message);
+        this.data=data;
+    }
+
+    public DataResult(T data,String message) {
+        super(message);
+        this.data=data;
+    }
+
+    public DataResult(T data,boolean success){
+        super(success);
+        this.data=data;
+    }
+}
