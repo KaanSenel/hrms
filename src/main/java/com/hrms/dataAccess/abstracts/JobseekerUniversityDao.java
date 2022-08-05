79654b4f05cc553b0e9b8c40bb1bef3e39a51f9b
Kaan Şenel <tufankaansenel@gmail.com>
2022-08-05 16:25:43 +0300

    9.gün ödevi cloudinary servisi eklenmesi hariç tamamlandı.

--- src/main/java/com/hrms/dataAccess/abstracts/JobseekerUniversityDao.java
+++ src/main/java/com/hrms/dataAccess/abstracts/JobseekerUniversityDao.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface JobseekerUniversityDao extends JpaRepository<JobseekerUniversity, Integer> {
+public interface JobseekerUniversityDao extends JpaRepository<JobseekerUniversity,Integer> {
+    List<JobseekerUniversity> getByJobseekerCv_Id(int id);
 
-    List<JobseekerUniversity> getByJobseekerCvId(int id);
+    List<JobseekerUniversity> getByJobseekerCvIdOrderByEndYearDesc(int id);
+
 }
